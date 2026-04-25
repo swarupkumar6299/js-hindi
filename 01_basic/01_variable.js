e79769c7251ab00fb27167ed8d26372a1e27e7f9
Swarup Kumar <87602927+swarupkumar6299@users.noreply.github.com>
2026-04-25 08:28:15 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "dear@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+accountEmail = "dr@ty.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
