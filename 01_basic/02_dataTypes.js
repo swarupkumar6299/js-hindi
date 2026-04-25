a31bd6c602fc89d1f015d7e1ec3f16e8a8cd44a8
Swarup Kumar <87602927+swarupkumar6299@users.noreply.github.com>
2026-04-25 08:28:33 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code newer version
+
+//alert("helo")
+//alert(3+3)
+console.log(3 + 3)
+ console.log("Hiii")
+
+ let name = "hello"
+ let age = 78
+ let isloggedIn = false
+ let state;
+ // number => 2 to pwr 53
+ //bigInt 
+ //String ==> ""
+ // boolean ==> true/ false
+ //null ==> standlone value
+ // undefined ==> standalone
+ //symbol ==> unique
+
+ //object
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);//object
